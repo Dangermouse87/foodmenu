aaed15e4d905e86fd3bd11df6dabbb9e4a800fc4
Rajan Ladwa <rladwa@gmail.com>
2023-07-23 21:22:51 +0900

    added update/delete functionality

--- food/urls.py
+++ food/urls.py
@@ -5,7 +5,8 @@ app_name = 'food'
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('item/', views.item, name='item'),
     path('<int:item_id>', views.detail, name='detail'),
     path('new/', views.new, name='new'),
+    path('edit/<int:pk>', views.update, name='update'),
+    path('delete/<int:pk>', views.delete, name='delete')
 ]
