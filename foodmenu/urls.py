47062083958f9042d2d31b068a2cda5c2ddacc3a
Rajan Ladwa <rladwa@gmail.com>
2023-07-24 23:11:36 +0900

    added default image

--- foodmenu/urls.py
+++ foodmenu/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views # namespace to avoid confusion
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,10 @@ urlpatterns = [
     path('signup', user_views.signup, name='signup'),
     path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'login'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name = 'logout'),
+    path('profile/', user_views.profile, name='profile'),
 ]
+
+
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
